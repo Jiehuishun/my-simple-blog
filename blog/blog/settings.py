1c555e366f96bcee2c61c37e7bb6aabb1a35d1a6
Jiehuishun <jiehuishun@outlook.com>
2021-03-21 23:55:45 +0800

    static resources configurated

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+#configuration of static resources
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 
 # configuration of redis 
